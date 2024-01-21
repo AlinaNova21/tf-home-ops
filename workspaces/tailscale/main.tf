af8cc376cf85253b344c0c1a5891243c4af4c119
Alina Shumann <ashumann13@gmail.com>
2024-01-20 22:45:42 -0600

    Remove do locked line entirely.

--- workspaces/tailscale/main.tf
+++ workspaces/tailscale/main.tf
@@ -4,7 +4,6 @@ resource "digitalocean_droplet" "digitalocean1" {
   size              = "s-1vcpu-512mb-10gb"
   region            = "nyc1"
   graceful_shutdown = true
-  # locked            = true
   tags = [
     "tf-home-ops",
     "tailscale",
