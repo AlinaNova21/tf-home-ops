841e416eeba0462479fb7164cd36f0258788838c
Alina Shumann <alina@alinanova.dev>
2024-09-21 00:21:49 -0500

    Update do

--- workspaces/tailscale/main.tf
+++ workspaces/tailscale/main.tf
@@ -21,6 +21,17 @@ resource "digitalocean_firewall" "digitalocean1" {
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "25565"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "udp"
+    port_range       = "25565"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
 
   outbound_rule {
     protocol              = "tcp"
